d4e700eb388d58ab2911f3682b5ebf5a4e96db00
AsiriRasnayaka <asirirasnayaka123@gmail.com>
2025-11-10 10:13:07 +0530

    update helper folder

---
+++ Backend/helper/uploadToCloudinary.js
@@ -0,0 +1,28 @@
+import fs from "fs";
+import path from "path";
+import cloudinary from "../config/cloudinary.js";
+
+const uploadToCloudinary = async (fileURLToPath, options = {}) => {
+    try{
+        //Get file Extension
+        const ext = path.extname(filePath).toLowerCase();
+
+        //Decide resource type
+        if(!options.resource_type) {
+            if ([".mp3", ".wav", ".aac", ".m4a", ".ogg"].includes(ext)) {
+                options.resource_type = "video";
+            } else {
+                options.resource_type = "image";
+            }
+        }
+        const result = await cloudinary.uploader.upload(filePath, options);
+        return result;
+    } catch (error) {
+        console.error("Error uploding file to cloudinary:", error);
+        throw error;
+    } finally {
+        fs.unlinkSync(filePath);
+    }
+};
+
+export default uploadToCloudinary;
